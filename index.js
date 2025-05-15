78439d2adbb36e5e58c32114737941482cd645e0
datcvo1 <datcvo1@gmail.com>
2025-05-15 10:16:41 -0700

    finished sketchpad

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ const container = document.querySelector("#container");
 
 const numColsRows = 16;
 
+let mouseHold = false;
+let rainbow = ['#e81416', '#ffa500', '#faeb36', '#79c314', '#487de7', '#4b369d', '#70369d'];
+let index = rainbow.length;
+
+document.onmousedown = () => mouseHold = true;
+document.onmouseup = () => mouseHold = false;
+
 function createDiv(row)
 {
     const div = document.createElement("div");
     div.setAttribute("class", "pixel");
+    div.style.opacity = 0;
+    
+    div.onmouseover = () => {
+        if(mouseHold)
+        {
+            div.style.backgroundColor = cycleRainbow();
+            if(div.style.opacity != '1')
+                div.style.opacity -= -0.1;
+        }
+    };
+    div.onmousedown = () => {
+        div.style.backgroundColor = cycleRainbow();
+        if(div.style.opacity != '1')
+            div.style.opacity -= -0.1;
+    }
+
     row.appendChild(div);
 }
 
@@ -24,7 +47,25 @@ function createRow(colsRows)
 
 }
 
+function cycleRainbow() {
+    if(++index >= rainbow.length)
+        index = 0;
+
+    return rainbow[index];
+}
+
+
 createRow(numColsRows);
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let input = 0;
+    while(input <= 0 || input > 100)
+        input = prompt("Enter amount of pixels per side (max 100):", 16);
+
+    container.innerHTML = '';
+    createRow(input);
+})
 
 
 
